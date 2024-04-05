916a33dceadc41c746a7e210d41edbec2e8ecf4f
Juyeong "James" Noh <juyeongnoh@gmail.com>
2024-04-05 20:27:10 +0900

    DISTANCE-24 design: 축제 페이지, 마이페이지 디자인 반영

--- src/pages/festival/FestivalIndexPage.jsx
+++ src/pages/festival/FestivalIndexPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 
@@ -13,50 +13,44 @@ const TabMenu = styled.nav`
   display: flex;
   gap: 1.5rem;
   align-items: center;
+  margin-bottom: 1.5rem;
 `;
 const Tab = styled.div`
   font-size: 20px;
   font-weight: 600;
   text-align: center;
-  border-bottom: ${props => props.$isSelected ? "3px solid #FF625D" : "3px solid transparent"};
+  border-bottom: ${(props) =>
+    props.$isSelected ? "3px solid #FF625D" : "3px solid transparent"};
   transition: all 200ms;
 `;
 
 const FestivalIndexPage = () => {
-
   const navigate = useNavigate();
   const [tabMenuState, setTabMenuState] = useState(0);
 
-  const handleOnProgram = () => {
+  const handleClickProgram = () => {
     navigate("/festival/program");
     setTabMenuState(0);
-  }
-  const handleOnSchedule = () => {
-    navigate("/festival/schedule");
+  };
+  const handleClickFoodTruck = () => {
+    navigate("/festival/foodtruck");
     setTabMenuState(1);
-  }
-  const handleOnInfo = () => {
-    navigate("/festival/information");
-    setTabMenuState(2);
-  }
+  };
 
   return (
     <FestivalContainer>
       <Header />
       <TabMenu>
-        <Tab
-          $isSelected={tabMenuState === 0}
-          onClick={handleOnProgram}>프로그램</Tab>
-        <Tab
-          $isSelected={tabMenuState === 1}
-          onClick={handleOnSchedule}>일정</Tab>
-        <Tab
-          $isSelected={tabMenuState === 2}
-          onClick={handleOnInfo}>공지사항</Tab>
+        <Tab $isSelected={tabMenuState === 0} onClick={handleClickProgram}>
+          프로그램
+        </Tab>
+        <Tab $isSelected={tabMenuState === 1} onClick={handleClickFoodTruck}>
+          푸드트럭
+        </Tab>
       </TabMenu>
-      <Outlet/>
+      <Outlet />
     </FestivalContainer>
-  )
+  );
 };
 
 export default FestivalIndexPage;
