0e9c86c66d4b872b2074a7f2ee89da30b820c539
Juyeong "James" Noh <juyeongnoh@gmail.com>
2024-04-11 01:13:21 +0900

    DISTANCE-54 feat: 상단바 아이콘 클릭 시 내 프로필 표시 모달

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,23 +4,63 @@ import { isLoggedInState } from "../../store/auth";
 import { Link } from "react-router-dom";
 import { myDataState } from "../../store/myData";
 import { CHARACTERS, COLORS } from "../../constants/character";
+import { useNavigate } from "react-router-dom";
+import Modal from "./Modal";
+import { useRef } from "react";
+import Badge from "./Badge";
 
 const Header = () => {
   const isLoggedIn = useRecoilValue(isLoggedInState);
   const myData = useRecoilValue(myDataState);
+  const modalRef = useRef();
+  const navigate = useNavigate();
 
   return (
-    <WrapHeader>
-      <img src="/assets/logo-pink.png" alt="디스턴스 로고" />
-
-      {isLoggedIn ? (
-        <ProfileIcon $character={myData.memberCharacter}>
-          <img src={CHARACTERS[myData.memberCharacter]} alt="프로필 이미지" />
-        </ProfileIcon>
-      ) : (
-        <StyledLink to="/login">로그인</StyledLink>
-      )}
-    </WrapHeader>
+    <>
+      <WrapHeader>
+        <img src="/assets/logo-pink.png" alt="디스턴스 로고" />
+        {isLoggedIn ? (
+          <ProfileIcon
+            $character={myData.memberCharacter}
+            onClick={() => {
+              console.log("clicked!");
+              modalRef.current.open();
+            }}>
+            <img src={CHARACTERS[myData.memberCharacter]} alt="프로필 이미지" />
+          </ProfileIcon>
+        ) : (
+          <StyledLink to="/login">로그인</StyledLink>
+        )}
+      </WrapHeader>
+      <Modal
+        ref={modalRef}
+        buttonLabel="프로필 수정하기"
+        buttonColor="#FFAC0B"
+        buttonClickHandler={() => {
+          navigate("/mypage/profile", { state: myData });
+        }}>
+        <WrapContent>
+          <CharacterBackground $character={myData.memberCharacter}>
+            <StyledImage
+              src={CHARACTERS[myData.memberCharacter]}
+              alt={myData.memberCharacter}
+            />
+          </CharacterBackground>
+          <TextDiv>
+            <MBTI>{myData.mbti}</MBTI>
+            <Major>{myData.department}</Major>
+          </TextDiv>
+          <TagContainer>
+            {myData.memberHobbyDto.map((hobby, index) => (
+              <Badge key={index}>#{hobby.hobby}</Badge>
+            ))}
+            {myData.memberTagDto.map((tag, index) => (
+              <Badge key={index}>#{tag.tag}</Badge>
+            ))}
+          </TagContainer>
+        </WrapContent>
+      </Modal>
+    </>
   );
 };
 
@@ -53,6 +93,57 @@ const ProfileIcon = styled.div`
     width: 32px;
     height: 32px;
   }
+`;
+
+const WrapContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 32px 0;
+  gap: 12px;
+`;
+
+const CharacterBackground = styled.div`
+  position: relative;
+  width: 60%;
+  height: 0;
+  padding-bottom: 60%;
+  border-radius: 50%;
+  background-color: ${(props) => COLORS[props.$character]};
+`;
+
+const StyledImage = styled.img`
+  position: absolute;
+  width: 60%;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const TextDiv = styled.div`
+  width: 100%;
+  text-align: center;
+  color: #333333;
+`;
+
+const Major = styled.div`
+  font-size: 24px;
+  font-weight: 700;
+  white-space: nowrap;
+`;
+
+const MBTI = styled.div`
+  color: #000000;
+  font-size: 14px;
+`;
+
+const TagContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
 `;
 
 export default Header;
