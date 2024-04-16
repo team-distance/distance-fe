61208424a99afe43ca2b09101759860a3af0b45a
Juyeong "James" Noh <juyeongnoh@gmail.com>
2024-04-16 22:13:58 +0900

    DISTANCE-118 feat: 인증 방식 refresh token으로 변경

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { baseURL } from "../constants/baseURL";
+import { refresh } from "../store/auth";
 
 export const instance = axios.create({
   baseURL: baseURL,
@@ -7,9 +8,35 @@ export const instance = axios.create({
 });
 
 instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  const accessToken = localStorage.getItem("accessToken");
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
   }
   return config;
 });
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const status = error?.response?.status;
+    const message = error?.response?.data?.message;
+
+    if (status === 401 && message === "만료된 JWT 토큰입니다!") {
+      await refresh({
+        refreshToken: localStorage.getItem("refreshToken"),
+      }).catch((error) => {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        window.location.href = "/login";
+        return Promise.reject(error);
+      });
+
+      const accessToken = localStorage.getItem("accessToken");
+      error.config.headers.Authorization = `Bearer ${accessToken}`;
+      return instance(error.config);
+    }
+    return Promise.reject(error);
+  }
+);
--- src/store/auth.js
+++ src/store/auth.js
@@ -1,9 +1,10 @@
 import { atom } from "recoil";
 import { instance } from "../api/instance";
+import axios from "axios";
 
 export const isLoggedInState = atom({
   key: "isLoggedInState",
-  default: localStorage.getItem("token") ? true : false,
+  default: localStorage.getItem("accessToken") ? true : false,
 });
 
 export const login = async (value) => {
@@ -13,7 +14,17 @@ export const login = async (value) => {
     clientToken: value.clientToken,
   });
 
-  const { token } = response.data;
+  const { accessToken, refreshToken } = response.data;
 
-  localStorage.setItem("token", token);
+  localStorage.setItem("accessToken", accessToken);
+  localStorage.setItem("refreshToken", refreshToken);
+};
+
+export const refresh = async (value) => {
+  const response = await axios.post("https://api.dis-tance.com/api/refresh", {
+    refreshToken: value.refreshToken,
+  });
+
+  const { accessToken } = response.data;
+  localStorage.setItem("accessToken", accessToken);
 };
