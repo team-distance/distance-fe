094ad1af76af4e176a1372a7706f9bd0817ecaac
Juyeong "James" Noh <juyeongnoh@gmail.com>
2024-04-17 08:08:31 +0900

    chore: 서비스 워커 백그라운드 메시지 처리 코드 주석 처리

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -25,54 +25,42 @@ self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
 });
 
-// self.addEventListener("push", (event) => {
-//   console.log("PUSH EVENT!", event.data.json());
-//   const payload = event.data.json();
+// messaging.onBackgroundMessage((payload) => {
+//   console.log("BACKGROUND MESSAGE RECEIVED", payload);
+
 //   const notificationTitle = payload.notification.title;
 //   const notificationOptions = {
-//     body: payload.notification.body,
+//     body: payload.notification.body, // 'payload'의 'notification.body'를 사용
 //   };
-//   event.waitUntil(
-//     self.registration.showNotification(notificationTitle, notificationOptions)
-//   );
+
+//   // 알림 표시
+//   // 백그라운드에서 자동으로 알림이 표시되어 주석 처리함.
+//   self.registration.showNotification(notificationTitle, notificationOptions);
 // });
 
-messaging.onBackgroundMessage((payload) => {
-  console.log("BACKGROUND MESSAGE RECEIVED", payload);
+// self.addEventListener("notificationclick", function (event) {
+//   console.log("Notification Clicked", event);
 
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body, // 'payload'의 'notification.body'를 사용
-  };
+//   // 알림 창 닫기
+//   event.preventDefault();
+//   event.notification.close();
 
-  // 알림 표시
-  // 백그라운드에서 자동으로 알림이 표시되어 주석 처리함.
-  // self.registration.showNotification(notificationTitle, notificationOptions);
-});
-
-self.addEventListener("notificationclick", function (event) {
-  console.log("Notification Clicked", event);
-
-  // 알림 창 닫기
-  event.preventDefault();
-  event.notification.close();
-
-  // '/chat' 라우트로 사용자를 이동시킵니다.
-  event.waitUntil(
-    clients.matchAll({ type: "window" }).then((windowClients) => {
-      // 이미 '/chat' 페이지가 열려있는 탭이 있는지 확인합니다.
-      for (var i = 0; i < windowClients.length; i++) {
-        var client = windowClients[i];
-        // 여기서는 단순화를 위해 모든 경로를 검사하지 않습니다.
-        // 실제로는 client.url을 검사하여 적절히 처리할 필요가 있을 수 있습니다.
-        if (client.url.includes("/chat") && "focus" in client) {
-          return client.focus();
-        }
-      }
-      // '/chat' 페이지가 열려있는 탭이 없으면 새 탭에서 엽니다.
-      if (clients.openWindow) {
-        return clients.openWindow("/chat");
-      }
-    })
-  );
-});
+//   // '/chat' 라우트로 사용자를 이동시킵니다.
+//   event.waitUntil(
+//     clients.matchAll({ type: "window" }).then((windowClients) => {
+//       // 이미 '/chat' 페이지가 열려있는 탭이 있는지 확인합니다.
+//       for (var i = 0; i < windowClients.length; i++) {
+//         var client = windowClients[i];
+//         // 여기서는 단순화를 위해 모든 경로를 검사하지 않습니다.
+//         // 실제로는 client.url을 검사하여 적절히 처리할 필요가 있을 수 있습니다.
+//         if (client.url.includes("/chat") && "focus" in client) {
+//           return client.focus();
+//         }
+//       }
+//       // '/chat' 페이지가 열려있는 탭이 없으면 새 탭에서 엽니다.
+//       if (clients.openWindow) {
+//         return clients.openWindow("/chat");
+//       }
+//     })
+//   );
+// });
