536a299e016b780cc88fcc6973012bdbc34e0a4f
Juyeong "James" Noh <juyeongnoh@gmail.com>
2024-04-22 17:09:57 +0900

    DISTANCE-134 feat: 통화 허용 로직 추가

--- src/components/chat/Message.jsx
+++ src/components/chat/Message.jsx
@@ -1,48 +1,134 @@
 import React, { memo } from 'react';
 import styled from 'styled-components';
 import { parseTime } from '../../utils/parseTime';
+import Button from '../common/Button';
 
 /**
  * @param {string} nickname - 메시지를 보낸 사람의 닉네임
  * @param {string} content - 메시지 내용
  * @param {string} time - 메시지를 보낸 시간
  * @param {boolean} read - 메시지를 읽었는지 여부
- * @param {string} senderType - 메시지를 보낸 사람의 타입 (SYSTEM, USER)
+ * @param {string} senderType - 메시지를 보낸 사람의 타입 (SYSTEM, USER, CALL_REQUEST, CALL_RESPONSE)
  * @param {boolean} sentByMe - 메시지를 내가 보냈는지 여부
  */
 const Message = memo(
-  ({ nickname, content, time, read, senderType, sentByMe }) => {
-    if (senderType === 'SYSTEM') {
-      return (
-        <Announcement>
-          <div className="content">{content}</div>
-        </Announcement>
-      );
-    } else if (senderType === 'USER') {
-      return sentByMe ? (
-        <MessageByMe>
-          <div className="message-container">
-            <div className="wrapper">
-              <div className="read">{read !== 0 ? read : ''}</div>
-              <div className="time">{parseTime(time)}</div>
+  ({ nickname, content, time, read, senderType, sentByMe, responseCall }) => {
+    switch (senderType) {
+      case 'SYSTEM':
+        return (
+          <Announcement>
+            <div className="content">{content}</div>
+          </Announcement>
+        );
+
+      case 'CALL_REQUEST':
+        return sentByMe ? (
+          <MessageByMe>
+            <div className="message-container">
+              <div className="wrapper">
+                <div className="read">{read !== 0 ? read : ''}</div>
+                <div className="time">{parseTime(time)}</div>
+              </div>
+              <div className="tail"></div>
+              <div className="message">
+                <strong>통화를 요청하셨어요!</strong>
+                <ul>
+                  <li>요청을 수락하면 서로의 번호로 통화할 수 있어요.</li>
+                </ul>
+              </div>
             </div>
-            <div className="tail"></div>
-            <div className="message">{content}</div>
-          </div>
-        </MessageByMe>
-      ) : (
-        <MessageByOther>
-          <div className="nickname">{nickname}</div>
-          <div className="message-container">
-            <div className="tail"></div>
-            <div className="message">{content}</div>
-            <div className="wrapper">
-              <div className="read">{read !== 0 ? read : ''}</div>
-              <div className="time">{parseTime(time)}</div>
+          </MessageByMe>
+        ) : (
+          <MessageByOther>
+            <div className="nickname">{nickname}</div>
+            <div className="message-container">
+              <div className="tail"></div>
+              <div className="message">
+                <strong>통화를 요청하셨어요!</strong>
+                <ul>
+                  <li>요청을 수락하면 서로의 번호로 통화할 수 있어요.</li>
+                </ul>
+                <Button
+                  size="large"
+                  backgroundColor="#18D736"
+                  onClick={responseCall}
+                >
+                  수락하기
+                </Button>
+              </div>
+              <div className="wrapper">
+                <div className="read">{read !== 0 ? read : ''}</div>
+                <div className="time">{parseTime(time)}</div>
+              </div>
             </div>
-          </div>
-        </MessageByOther>
-      );
+          </MessageByOther>
+        );
+
+      case 'CALL_RESPONSE':
+        return sentByMe ? (
+          <MessageByMe>
+            <div className="message-container">
+              <div className="wrapper">
+                <div className="read">{read !== 0 ? read : ''}</div>
+                <div className="time">{parseTime(time)}</div>
+              </div>
+              <div className="tail"></div>
+              <div className="message">
+                <strong>통화 요청을 수락했어요!</strong>
+                <div>
+                  상단 <img src="/assets/callicon-active.png" alt="전화버튼" />
+                  버튼을 눌러 통화해보세요.
+                </div>
+              </div>
+            </div>
+          </MessageByMe>
+        ) : (
+          <MessageByOther>
+            <div className="nickname">{nickname}</div>
+            <div className="message-container">
+              <div className="tail"></div>
+              <div className="message">
+                <strong>통화 요청을 수락했어요!</strong>
+                <div>
+                  상단 <img src="/assets/callicon-active.png" alt="전화버튼" />
+                  버튼을 눌러 통화해보세요.
+                </div>
+              </div>
+              <div className="wrapper">
+                <div className="read">{read !== 0 ? read : ''}</div>
+                <div className="time">{parseTime(time)}</div>
+              </div>
+            </div>
+          </MessageByOther>
+        );
+
+      case 'USER':
+        return sentByMe ? (
+          <MessageByMe>
+            <div className="message-container">
+              <div className="wrapper">
+                <div className="read">{read !== 0 ? read : ''}</div>
+                <div className="time">{parseTime(time)}</div>
+              </div>
+              <div className="tail"></div>
+              <div className="message">{content}</div>
+            </div>
+          </MessageByMe>
+        ) : (
+          <MessageByOther>
+            <div className="nickname">{nickname}</div>
+            <div className="message-container">
+              <div className="tail"></div>
+              <div className="message">{content}</div>
+              <div className="wrapper">
+                <div className="read">{read !== 0 ? read : ''}</div>
+                <div className="time">{parseTime(time)}</div>
+              </div>
+            </div>
+          </MessageByOther>
+        );
+      default:
+        return null;
     }
   }
 );
