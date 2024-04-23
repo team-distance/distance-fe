6a6b7b1492ef6cf7b88c159829af1a03d9afaac0
Juyeong "James" Noh <juyeongnoh@gmail.com>
2024-04-24 00:39:24 +0900

    DISTANCE-148 chore: 서버에서 가져온 distance가 -1이면 툴팁과 함께 위치 표시할 수 없다는 메시지 출력

--- src/pages/chat/ChatPage.jsx
+++ src/pages/chat/ChatPage.jsx
@@ -13,6 +13,7 @@ import Lottie from 'react-lottie-player';
 import callAnimation from '../../lottie/call-animation.json';
 import useGroupedMessages from '../../hooks/useGroupedMessages';
 import Modal from '../../components/common/Modal';
+import Tooltip from '../../components/common/Tooltip';
 
 const ChatPage = () => {
   const [client, setClient] = useState(null);
@@ -375,7 +376,14 @@ const ChatPage = () => {
           <WrapTitle>
             <div className="title">상대방과의 거리</div>
             <div className="subtitle">
-              {distance === -1 ? '불러오는 중...' : `${distance}m`}
+              {distance === -1 ? (
+                <>
+                  <Tooltip message="두 명 모두 위치 정보를 공유해야 확인할 수 있어요!" />{' '}
+                  <span>위치를 표시할 수 없습니다.</span>
+                </>
+              ) : (
+                `${distance}m`
+              )}
             </div>
           </WrapTitle>
           <div>
@@ -519,6 +527,7 @@ const TopBar = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  z-index: 9999;
 `;
 
 const ModalContent = styled.div`
