60c6186228a382a1e1d814afd5c27fc253083ae4
Solmi <7548ck@naver.com>
2024-04-30 23:51:48 +0900

    chore: build error 해결

--- src/pages/register/UserRegisterPage.jsx
+++ src/pages/register/UserRegisterPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
 import TextInput from '../../components/register/TextInput';
 import { useRecoilState } from 'recoil';
