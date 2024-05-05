51c21d7feac3464d343ff34c8d050025db0b2ed9
Juyeong "James" Noh <juyeongnoh@gmail.com>
2024-05-05 15:12:42 +0900

    chore: RUM 연동

--- src/index.js
+++ src/index.js
@@ -5,16 +5,28 @@ import reportWebVitals from './reportWebVitals';
 import { RecoilRoot } from 'recoil';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { AwsRumProvider } from 'aws-rum-react';
 
 const rootElement = document.getElementById('root');
 
 const app = (
   <React.StrictMode>
-    <RecoilRoot>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </RecoilRoot>
+    <AwsRumProvider
+      allowCookies
+      endpoint={process.env.REACT_APP_AWS_RUM_ENDPOINT}
+      id={process.env.REACT_APP_AWS_RUM_ID}
+      identityPoolId={process.env.REACT_APP_AWS_IDENTITY_POOL_ID}
+      region="ap-northeast-2"
+      sessionSampleRate={1}
+      telemetries={['performance', 'errors', 'http']}
+      version="1.0.0"
+    >
+      <RecoilRoot>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </RecoilRoot>
+    </AwsRumProvider>
   </React.StrictMode>
 );
 const root = createRoot(rootElement);
