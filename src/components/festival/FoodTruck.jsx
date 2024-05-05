acd2234be87e8d3271db58af91c36f96b27ce644
Solmi <7548ck@naver.com>
2024-05-06 04:43:30 +0900

    feat: 푸드트럭 배포용 화면 구현

--- src/components/festival/FoodTruck.jsx
+++ src/components/festival/FoodTruck.jsx
@@ -1,36 +1,51 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import FoodTruckCard from './FoodTruckCard';
-import { useNavigate } from 'react-router-dom';
-import { instance } from '../../api/instance';
-import ClipLoader from 'react-spinners/ClipLoader';
+// import FoodTruckCard from './FoodTruckCard';
+// import { useNavigate } from 'react-router-dom';
+// import { instance } from '../../api/instance';
+// import ClipLoader from 'react-spinners/ClipLoader';
 
 const FoodTruck = () => {
-  const navigate = useNavigate();
-  const [foodTruckList, setFoodTruckList] = useState([]);
-  const [loading, setLoading] = useState(false);
+  // const navigate = useNavigate();
+  // const [foodTruckList, setFoodTruckList] = useState([]);
+  // const [loading, setLoading] = useState(false);
 
-  const fetchFoodtruckInfo = async () => {
-    let school = '순천향대학교';
-    try {
-      setLoading(true);
-      const res = await instance.get(`/food-truck?school=${school}`);
-      setFoodTruckList(res.data);
-      console.log('res', res.data);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
+  // const fetchFoodtruckInfo = async () => {
+  //   let school = '순천향대학교';
+  //   try {
+  //     setLoading(true);
+  //     const res = await instance.get(`/food-truck?school=${school}`);
+  //     setFoodTruckList(res.data);
+  //     console.log('res', res.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchFoodtruckInfo();
+  // }, []);
+
+  //디데이
+  const calculateDDay = (targetDate) => {
+    const today = new Date();
+    const target = new Date(targetDate);
+    const daysLeft = target.getDate() - today.getDate();
+
+    if (daysLeft < 1) {
+      return 'Day';
     }
+
+    return daysLeft;
   };
 
-  useEffect(() => {
-    fetchFoodtruckInfo();
-  }, []);
+  const dDay = calculateDDay('2024-05-07');
 
   return (
     <>
-      {loading ? (
+      {/* {loading ? (
         <LoaderContainer>
           <ClipLoader color={'#FF625D'} loading={loading} size={50} />
         </LoaderContainer>
@@ -47,27 +62,66 @@ const FoodTruck = () => {
             />
           ))}
         </WrapCards>
-      )}
+      )} */}
+      <EmptyContainer>
+        <div className="wrap">
+          <img src={'/assets/empty-festival.svg'} alt="empty icon" />
+          <div>피닉시아 축제까지</div>
+          <span>D-{dDay}</span>
+        </div>
+      </EmptyContainer>
     </>
   );
 };
 
 export default FoodTruck;
 
-const WrapCards = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`;
+// const WrapCards = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 1rem;
+// `;
 
-const LoaderContainer = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+// const LoaderContainer = styled.div`
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+//   width: 100%;
+//   height: 100%;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   z-index: 999;
+// `;
+
+const EmptyContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 999;
+  width: 100%;
+  height: 65vh;
+
+  > .wrap {
+    text-align: center;
+
+    > img {
+      margin-bottom: 1rem;
+    }
+
+    > div {
+      color: #333333;
+      text-align: center;
+      font-size: 1.2rem;
+      font-weight: 800;
+    }
+
+    > span {
+      background: linear-gradient(#ff3f38 0%, #ffb1ae 100%);
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
+      font-size: 2rem;
+      font-weight: 600;
+    }
+  }
 `;
--- src/pages/festival/FestivalIndexPage.jsx
+++ src/pages/festival/FestivalIndexPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useState } from 'react';
 import styled from 'styled-components';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
