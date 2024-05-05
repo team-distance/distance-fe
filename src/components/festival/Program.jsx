cb205023e79023c6754cc6cfb39ee2a38cfe407e
Solmi <7548ck@naver.com>
2024-05-06 04:34:54 +0900

    feat: 축제 정보 페이지에 스피너 구현

--- src/components/festival/Program.jsx
+++ src/components/festival/Program.jsx
@@ -2,6 +2,72 @@ import styled from 'styled-components';
 import ProgramCard from './ProgramCard';
 import { useEffect, useState } from 'react';
 import { instance } from '../../api/instance';
+import ClipLoader from 'react-spinners/ClipLoader';
+
+const Program = () => {
+  const [programList, setProgramList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchProgramInfo = async () => {
+    let school = '순천향대학교';
+    try {
+      setLoading(true);
+      const res = await instance.get(`/performance?school=${school}`);
+      setProgramList(res.data);
+      console.log('res', res.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProgramInfo();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <LoaderContainer>
+          <ClipLoader color={'#FF625D'} loading={loading} size={50} />
+        </LoaderContainer>
+      ) : (
+        <>
+          <Title>순천향대학교</Title>
+          <Date>5월 7일</Date>
+          <WrapCards>
+            {programList.map(
+              (program) =>
+                program.startAt.startsWith('2024-05-07') && (
+                  <ProgramCard content={program} />
+                )
+            )}
+          </WrapCards>
+          <Date className="cardsDate">5월 8일</Date>
+          <WrapCards>
+            {programList.map(
+              (program) =>
+                program.startAt.startsWith('2024-05-08') && (
+                  <ProgramCard content={program} />
+                )
+            )}
+          </WrapCards>
+          <Date className="cardsDate">5월 9일</Date>
+          <WrapCards>
+            {programList.map(
+              (program) =>
+                program.startAt.startsWith('2024-05-09') && (
+                  <ProgramCard content={program} />
+                )
+            )}
+          </WrapCards>
+        </>
+      )}
+    </>
+  );
+};
+export default Program;
 
 const Title = styled.div`
   font-size: 36px;
@@ -18,55 +84,14 @@ const WrapCards = styled.div`
   gap: 1rem;
 `;
 
-const Program = () => {
-  const [programList, setProgramList] = useState([]);
-
-  const fetchProgramInfo = async () => {
-    let school = '순천향대학교';
-    try {
-      const res = await instance.get(`/performance?school=${school}`);
-      setProgramList(res.data);
-      console.log('res', res.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchProgramInfo();
-  }, []);
-
-  return (
-    <>
-      <Title>순천향대학교</Title>
-      <Date>5월 7일</Date>
-      <WrapCards>
-        {programList.map(
-          (program) =>
-            program.startAt.startsWith('2024-05-07') && (
-              <ProgramCard content={program} />
-            )
-        )}
-      </WrapCards>
-      <Date className="cardsDate">5월 8일</Date>
-      <WrapCards>
-        {programList.map(
-          (program) =>
-            program.startAt.startsWith('2024-05-08') && (
-              <ProgramCard content={program} />
-            )
-        )}
-      </WrapCards>
-      <Date className="cardsDate">5월 9일</Date>
-      <WrapCards>
-        {programList.map(
-          (program) =>
-            program.startAt.startsWith('2024-05-09') && (
-              <ProgramCard content={program} />
-            )
-        )}
-      </WrapCards>
-    </>
-  );
-};
-export default Program;
+const LoaderContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+`;
