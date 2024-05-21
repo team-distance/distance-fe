c9e78e11d6ee1160ed8ca04b3758b412cc1d0cb0
Juyeong "James" Noh <juyeongnoh@gmail.com>
2024-05-21 21:16:25 +0900

    chore: 축제 D-day 표시

--- src/components/festival/FoodTruck.jsx
+++ src/components/festival/FoodTruck.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import FoodTruckCard from './FoodTruckCard';
-import { useNavigate } from 'react-router-dom';
+// import FoodTruckCard from './FoodTruckCard';
+// import { useNavigate } from 'react-router-dom';
 import { instance } from '../../api/instance';
-import ClipLoader from 'react-spinners/ClipLoader';
+// import ClipLoader from 'react-spinners/ClipLoader';
 import { UNIV_STATE } from '../../constants/collegeState';
 
 const FoodTruck = () => {
-  const navigate = useNavigate();
-  const [foodTruckList, setFoodTruckList] = useState([]);
-  const [loading, setLoading] = useState(false);
+  // const navigate = useNavigate();
+  // const [foodTruckList, setFoodTruckList] = useState([]);
+  // const [loading, setLoading] = useState(false);
   const [school, setSchool] = useState('');
 
   useEffect(() => {
@@ -26,41 +26,41 @@ const FoodTruck = () => {
     getDomain();
   }, []);
 
-  useEffect(() => {
-    const fetchFoodtruckInfo = async () => {
-      if (school === '') return;
-      try {
-        setLoading(true);
-        const res = await instance.get(`/food-truck?school=${school}`);
-        setFoodTruckList(res.data);
-        console.log('res', res.data);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchFoodtruckInfo = async () => {
+  //     if (school === '') return;
+  //     try {
+  //       setLoading(true);
+  //       const res = await instance.get(`/food-truck?school=${school}`);
+  //       setFoodTruckList(res.data);
+  //       console.log('res', res.data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    fetchFoodtruckInfo();
-  }, [school]);
+  //   fetchFoodtruckInfo();
+  // }, [school]);
 
-  // const calculateDDay = (targetDate) => {
-  //   const today = new Date();
-  //   const target = new Date(targetDate);
-  //   const daysLeft = target.getDate() - today.getDate();
+  const calculateDDay = (targetDate) => {
+    const today = new Date();
+    const target = new Date(targetDate);
+    const daysLeft = target.getDate() - today.getDate();
 
-  //   if (daysLeft < 1) {
-  //     return 'Day';
-  //   }
+    if (daysLeft < 1) {
+      return 'Day';
+    }
 
-  //   return daysLeft;
-  // };
+    return daysLeft;
+  };
 
-  // const dDay = calculateDDay('2024-05-07');
+  const dDay = calculateDDay('2024-05-22');
 
   return (
     <>
-      {loading ? (
+      {/* {loading ? (
         <LoaderContainer>
           <ClipLoader color={'#FF625D'} loading={loading} size={50} />
         </LoaderContainer>
@@ -78,66 +78,69 @@ const FoodTruck = () => {
             />
           ))}
         </WrapCards>
-      )}
-      {/* <EmptyContainer>
+      )} */}
+      <EmptyContainer>
         <div className="wrap">
           <img src={'/assets/empty-festival.svg'} alt="empty icon" />
-          <div>피닉시아 축제까지</div>
+          <div>
+            {school === '경희대학교' ? 'MASTERPEACE : Highlight' : '서랑제'}{' '}
+            축제까지
+          </div>
           <span>D-{dDay}</span>
         </div>
-      </EmptyContainer> */}
+      </EmptyContainer>
     </>
   );
 };
 
 export default FoodTruck;
 
-const WrapCards = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`;
+// const WrapCards = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 1rem;
+// `;
 
-const LoaderContainer = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
-`;
-
-// const EmptyContainer = styled.div`
+// const LoaderContainer = styled.div`
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+//   width: 100%;
+//   height: 100%;
 //   display: flex;
 //   justify-content: center;
 //   align-items: center;
-//   width: 100%;
-//   height: 65vh;
-
-//   > .wrap {
-//     text-align: center;
-
-//     > img {
-//       margin-bottom: 1rem;
-//     }
-
-//     > div {
-//       color: #333333;
-//       text-align: center;
-//       font-size: 1.2rem;
-//       font-weight: 800;
-//     }
-
-//     > span {
-//       background: linear-gradient(#ff3f38 0%, #ffb1ae 100%);
-//       background-clip: text;
-//       -webkit-background-clip: text;
-//       color: transparent;
-//       font-size: 2rem;
-//       font-weight: 600;
-//     }
-//   }
+//   z-index: 999;
 // `;
+
+const EmptyContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 65vh;
+
+  > .wrap {
+    text-align: center;
+
+    > img {
+      margin-bottom: 1rem;
+    }
+
+    > div {
+      color: #333333;
+      text-align: center;
+      font-size: 1.2rem;
+      font-weight: 800;
+    }
+
+    > span {
+      background: linear-gradient(#ff3f38 0%, #ffb1ae 100%);
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
+      font-size: 2rem;
+      font-weight: 600;
+    }
+  }
+`;
--- src/pages/festival/FestivalIndexPage.jsx
+++ src/pages/festival/FestivalIndexPage.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Outlet, useNavigate } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { tabState } from '../../store/tabState';
+import { isLoggedInState } from '../../store/auth';
 
 const TabMenu = styled.nav`
   display: flex;
@@ -20,8 +21,32 @@ const Tab = styled.div`
   transition: all 200ms;
 `;
 
+const EmptyContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 72vh;
+
+  > .wrap {
+    text-align: center; // 텍스트를 중앙 정렬합니다.
+
+    > img {
+      margin-bottom: 1rem; // 아이콘과 텍스트 사이의 간격을 조정합니다.
+    }
+
+    > div {
+      color: #333333;
+      text-align: center;
+      font-size: 18px;
+      font-weight: 700;
+    }
+  }
+`;
+
 const FestivalIndexPage = () => {
   const navigate = useNavigate();
+  const isLoggedIn = useRecoilValue(isLoggedInState);
 
   const [tabMenuState, setTabMenuState] = useRecoilState(tabState);
 
@@ -36,15 +61,29 @@ const FestivalIndexPage = () => {
 
   return (
     <>
-      <TabMenu>
-        <Tab $isSelected={tabMenuState === 0} onClick={handleClickProgram}>
-          프로그램
-        </Tab>
-        <Tab $isSelected={tabMenuState === 1} onClick={handleClickFoodTruck}>
-          푸드트럭
-        </Tab>
-      </TabMenu>
-      <Outlet />
+      {isLoggedIn ? (
+        <>
+          <TabMenu>
+            <Tab $isSelected={tabMenuState === 0} onClick={handleClickProgram}>
+              프로그램
+            </Tab>
+            <Tab
+              $isSelected={tabMenuState === 1}
+              onClick={handleClickFoodTruck}
+            >
+              푸드트럭
+            </Tab>
+          </TabMenu>
+          <Outlet />
+        </>
+      ) : (
+        <EmptyContainer>
+          <div className="wrap">
+            <img src={'/assets/access-denied-mypage.svg'} alt="access denied" />
+            <div>로그인 후 이용해주세요!</div>
+          </div>
+        </EmptyContainer>
+      )}
     </>
   );
 };
