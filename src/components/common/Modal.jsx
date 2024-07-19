26e7dca61c85a6f99232783ac0d5efe6bafbc2db
Juyeong "James" Noh <juyeongnoh@gmail.com>
2024-07-19 22:12:54 +0900

    feat: 홈화면 프로필 모달 구현

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -3,38 +3,6 @@ import { useRef, forwardRef, useImperativeHandle } from 'react';
 import Button from './Button';
 import { createPortal } from 'react-dom';
 
-const StyledDialog = styled.dialog`
-  width: 60%;
-  max-height: 80%;
-  padding: 32px;
-  position: relative;
-  border: none;
-  border-radius: 30px;
-  background: #ffffff;
-  box-shadow: 0px 0px 20px 0px rgba(255, 255, 255, 0.08);
-  z-index: 999;
-
-  &[open]::backdrop {
-    background: rgba(0, 0, 0, 0.5);
-    overflow-y: hidden;
-  }
-`;
-
-const CloseButton = styled.img`
-  flex: 1;
-  position: absolute;
-  top: 25px;
-  right: 32px;
-`;
-
-const WrapContent = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-
 const Modal = forwardRef(
   (
     { children, buttonLabel, buttonClickHandler, buttonColor = '#FF625D' },
@@ -91,3 +59,35 @@ const Modal = forwardRef(
 );
 
 export default Modal;
+
+const StyledDialog = styled.dialog`
+  width: 60%;
+  max-height: 80%;
+  padding: 32px;
+  position: relative;
+  border: none;
+  border-radius: 30px;
+  background: #ffffff;
+  box-shadow: 0px 0px 20px 0px rgba(255, 255, 255, 0.08);
+  z-index: 999;
+
+  &[open]::backdrop {
+    background: rgba(0, 0, 0, 0.5);
+    overflow-y: hidden;
+  }
+`;
+
+const CloseButton = styled.img`
+  flex: 1;
+  position: absolute;
+  top: 25px;
+  right: 32px;
+`;
+
+const WrapContent = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
