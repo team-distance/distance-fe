1f623bcc79c9cfd085467e1ff219ec9b4e3275d1
Parksolmi <goorm@goormui-MacBookPro.local>
2024-07-29 17:14:01 +0900

    feat: 토스트 메세지 커스텀 훅 만들기

---
+++ src/hooks/useToast.jsx
@@ -0,0 +1,14 @@
+import toast from 'react-hot-toast';
+
+/**
+ * @param {function} toast - 토스트 JSX를 반환하는 함수
+ * @param {string} message - 토스트 메세지 내용
+ * @param {object} position - 
+ * @returns {object} clickToast와 clickBackdrop 함수를 포함하는 객체
+ */
+
+const useToast = (toast, ) => {
+
+};
+
+export default useToast;
