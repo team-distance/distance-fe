91d35b73818e48d8a6f141fc2d4edd2cce5a2e03
Juyeong "James" Noh <juyeongnoh@gmail.com>
2024-10-15 21:39:13 +0900

    refactor: 채팅방 이미지 CDN으로 교체

--- src/components/chat/ImageView.jsx
+++ src/components/chat/ImageView.jsx
@@ -1,61 +1,69 @@
-import { useState } from "react";
-import styled from "styled-components"
-import { parseDate } from "../../utils/parseDate";
-import axios from "axios";
-import { usePromiseToast } from "../../hooks/useToast";
+import { useState } from 'react';
+import styled from 'styled-components';
+import { parseDate } from '../../utils/parseDate';
+import axios from 'axios';
+import { usePromiseToast } from '../../hooks/useToast';
 
 const ImageView = ({ imgSrc, handleCancel }) => {
-    const [showButton, setShowButton] = useState(true);
+  const [showButton, setShowButton] = useState(true);
 
-    const { showPromiseToast: showDownloadImageToast } = usePromiseToast();
+  const { showPromiseToast: showDownloadImageToast } = usePromiseToast();
 
-    const handleDownload = () => {
+  const handleDownload = () => {
+    try {
+      const res = axios.get(imgSrc, { responseType: 'blob' });
 
-        const cloudFrontURL = `https://d2ujunquh0apez.cloudfront.net/${imgSrc.replace('https://distance-buckets.s3.ap-northeast-2.amazonaws.com/', '')}`
+      showDownloadImageToast(
+        res,
+        (res) => {
+          const blobURL = URL.createObjectURL(res.data);
+          const aTag = document.createElement('a');
 
-        try {
-            const res = axios.get(cloudFrontURL, { responseType: 'blob' });
+          const date = parseDate(new Date());
 
-            showDownloadImageToast(res, (res) => {
-                const blobURL = URL.createObjectURL(res.data);
-                const aTag = document.createElement("a");
+          aTag.href = blobURL;
+          aTag.download = `distance/${date}.jpg`;
 
-                const date = parseDate(new Date());
+          aTag.click();
 
-                aTag.href = blobURL;
-                aTag.download = `Distance/${date}.jpg`;
-
-                aTag.click();
-
-                return '이미지가 저장되었습니다.';
-            },
-                (error) => {
-                console.log(error);
-                return "이미지 다운로드를 다시 시도해주세요.";
-            })
-        } catch (e) {
-            console.log(e);
+          return '이미지가 저장되었습니다.';
+        },
+        (error) => {
+          console.log(error);
+          return '이미지 다운로드를 다시 시도해주세요.';
         }
-    };
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-    return (
-        <>
-            {showButton &&
-                <WrapButtons>
-                    <div className="container" >
-                        <img src="/assets/chat/download-button.svg" alt="download" onClick={handleDownload} />
-                        <img src="/assets/chat/cancel-button.svg" alt="download" onClick={handleCancel} />
-                    </div>
-                </WrapButtons>
-            }
-            <Background onClick={() => setShowButton(prev => !prev)}>
-                <WrapImage>
-                    <img src={imgSrc} alt="view" />
-                </WrapImage>
-            </Background>
-        </>
-    )
-}
+  return (
+    <>
+      {showButton && (
+        <WrapButtons>
+          <div className="container">
+            <img
+              src="/assets/chat/download-button.svg"
+              alt="download"
+              onClick={handleDownload}
+            />
+            <img
+              src="/assets/chat/cancel-button.svg"
+              alt="download"
+              onClick={handleCancel}
+            />
+          </div>
+        </WrapButtons>
+      )}
+      <Background onClick={() => setShowButton((prev) => !prev)}>
+        <WrapImage>
+          <img src={imgSrc + '?f=webp'} alt="view" />
+        </WrapImage>
+      </Background>
+    </>
+  );
+};
 
 const Background = styled.div`
   position: fixed;
@@ -66,34 +74,33 @@ const Background = styled.div`
 
   background: black;
   z-index: 999;
-
 `;
 
 const WrapButtons = styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    background: rgba(0, 0, 0, 0.5);
-    z-index: 1000;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
 
-    .container {
-        display: flex;
-        justify-content: space-between;
-        padding: 1.5rem 1rem;
-    }
+  .container {
+    display: flex;
+    justify-content: space-between;
+    padding: 1.5rem 1rem;
+  }
 `;
 
 const WrapImage = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 
-    img {
-        max-width: 100%;
-        max-height: 100%;
-    }
+  img {
+    max-width: 100%;
+    max-height: 100%;
+  }
 `;
 
 export default ImageView;
--- src/components/chat/Message.jsx
+++ src/components/chat/Message.jsx
@@ -194,6 +194,11 @@ const Message = memo(
           </MessageByOther>
         );
       case 'IMAGE':
+        const imageViaCdn = message.chatMessage.replace(
+          'https://distance-buckets.s3.ap-northeast-2.amazonaws.com',
+          'https://cdn.dis-tance.com'
+        );
+
         return isSentByMe ? (
           <MessageByMe>
             <div className="message-container">
@@ -204,9 +209,9 @@ const Message = memo(
                 <div className="time">{parseTime(message.sendDt)}</div>
               </div>
               <img
-                src={message.chatMessage}
+                src={imageViaCdn + '?w=600&f=webp&q=75'}
                 alt="message"
-                onClick={() => viewImage(message.chatMessage)}
+                onClick={() => viewImage(imageViaCdn)}
               />
             </div>
           </MessageByMe>
@@ -225,9 +230,9 @@ const Message = memo(
               <div className="nickname">{message.senderName}</div>
               <div className="message-container">
                 <img
-                  src={message.chatMessage}
+                  src={imageViaCdn + '?w=600&f=webp&q=75'}
                   alt="message"
-                  onClick={() => viewImage(message.chatMessage)}
+                  onClick={() => viewImage(imageViaCdn)}
                 />
                 <div className="wrapper">
                   <div className="read">
